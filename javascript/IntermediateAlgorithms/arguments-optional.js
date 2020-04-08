8cf479960da9493330fe200e544faad724afb253
AJ <ancheetah@users.noreply.github.com>
2020-04-07 23:10:31 -0400

    changed if/else to shorthand

--- javascript/IntermediateAlgorithms/arguments-optional.js
+++ javascript/IntermediateAlgorithms/arguments-optional.js
@@ -16,24 +16,21 @@ If either argument isn't a valid number, return undefined.
 function addTogether() {
 
     let param = [...arguments];
+    //console.log(param);
 
     //Validate arguments
     let validArg = param.every(a => {
         return typeof(a) == "number" ? true : false;
     });
 
-    if (!validArg) {
-        return undefined;
-    }
+    return !validArg ? undefined 
+        : param.length > 1 ? param.reduce((sum, x) => sum += x, 0) //Calculate sum
+        : x => addTogether(...param, x); //If only one argument is passed
 
-    //Calculate sum
-    if (param.length > 1) {
-        return param.reduce( (sum, x) => sum += x, 0);
-    }
-    //If only one argument is supplied
-    else {
-        return x => addTogether(...param, x); 
-    }
+    /******************** NOTE TO SELF ***********
+      I keep forgetting to put "return" BEFORE the if/else statements. It needs to go outside of the conditionals so that something is returned from the conditional statement and passed to the function as a return value.
+    /*********************************************
 }
 
 console.log(addTogether(2)(9));
+console.log(addTogether(4,6));

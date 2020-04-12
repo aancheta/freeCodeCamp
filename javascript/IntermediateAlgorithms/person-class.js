b82c04a54d8a66c03889e007e398206d6f450013
AJ <ancheetah@users.noreply.github.com>
2020-04-12 10:56:52 -0400

    cleaner code; uses a private function to initialize variables

--- javascript/IntermediateAlgorithms/person-class.js
+++ javascript/IntermediateAlgorithms/person-class.js
@@ -2,15 +2,20 @@
 Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
 */
 
-//Solution 1: Regex method
-
 var Person = function(firstAndLast) {
   
   //Initialize private variables
-  let firstName = firstAndLast.match(/^\w+/)[0];
-  let lastName = firstAndLast.match(/\w+$/)[0];
-  let fullName = firstAndLast;
+  let firstName, lastName, fullName;
 
+  function splitNames(full) {
+    let nameArr = full.split(" ");
+    fullName = `${nameArr[0]} ${nameArr[1]}`;
+    firstName = nameArr[0];
+    lastName = nameArr[1];
+  }
+  
+  splitNames(firstAndLast);
+  
   //Get functions
   this.getFirstName = () => firstName;
   this.getLastName = () => lastName;
@@ -19,29 +24,36 @@ var Person = function(firstAndLast) {
   //Set functions
   this.setFirstName = (first) => {
     firstName = first;
-    fullName = firstName.concat(" ",lastName);
+    fullName = `${firstName} ${lastName}`;
   }
   this.setLastName = (last) => {
     lastName = last;
-    fullName = firstName.concat(" ", lastName);
+    fullName = `${firstName} ${lastName}`;
   }
   this.setFullName = (firstAndLast) => {
-    fullName = firstAndLast; 
-    firstName = firstAndLast.match(/^\w+/)[0];
-    lastName = firstAndLast.match(/\w+$/)[0];
+    splitNames(firstAndLast);
   }
 
 };
 
 var bob = new Person('Bob Ross');
+
+//Tests
+
 console.log(Object.keys(bob).length);
 console.log(bob instanceof Person);
 
 console.log(bob.getFirstName());
+console.log(bob.getLastName());
 console.log(bob.getFullName());
 
 bob.setFirstName("Haskell");
 console.log(bob.getFullName());
 
+bob.setLastName("Curry");
+console.log(bob.getFullName());
+
 bob.setFullName("Haskell Curry");
+console.log(bob.getFullName());
+console.log(bob.getFirstName());
 console.log(bob.getLastName());

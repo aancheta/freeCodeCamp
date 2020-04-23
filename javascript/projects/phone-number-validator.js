5a2db0d5a5299964da4bee90619e25c498e06edf
AJ <ancheetah@users.noreply.github.com>
2020-04-23 13:00:06 -0400

    more concise code & why my solution is better than fcc

--- javascript/projects/phone-number-validator.js
+++ javascript/projects/phone-number-validator.js
@@ -1,44 +1,31 @@
 // Telephone Number Validator
 
 function telephoneCheck(str) {
-    //The only special word characters allowed are "(", "-", " "
-    //Check for non-allowed characters
-    let rex1 = /[^-()\s0-9]/;
+    let rex1 = /^(1\s?)?\d{3}([-\s]?)\d{3}\2\d{4}$/,    //phone number w/o parentheses
+        // (1\s?)? allows for "1" or "1 "
+        // \d{3} look for a grouping of 3 digits
+        // \d{4}$ must end with a grouping of 4 digits
+        // [-\s]? allows for a delimiter of "-" or space
+        // \2 repeat the second group ([-\s]?) here
+        rex2 = /^(1\s?)?\(\d{3}\)\s?\d{3}[-\s]?\d{4}$/; //phone number w/parentheses
+        // \s? must have a space or else nothing here
+
     if (rex1.test(str)) {
-        return false;
+        return true;
     }
-
-    //Phone numbers that start with "1" are potentially valid
-    if (str[0] == "1" && str.match(/[0-9]/g).length == 11) {
-        return telephoneCheck(str.slice(1).trim());
-    }
-
-    //Check for valid number groupings ### ### ####
-    let rex2 = /^([(]{0,1})\d{3}(.{0,2})\d{3}(.{0,1})\d{4}$/; //also checks for single "(" at start
-    let delArr = str.match(rex2);   // captures delimiters separating numbers
-                                    // e.g. delArr = ["(123) 456-7890", "(", ") ", "-"]
-    if (!delArr) {
-        return false;
-    }
-
-    switch (delArr[1]) {
-        case "":
-            //Case 1: First character in str is a number. 3 valid formats.
-            //5555555555 || 555 555 5555 || 555-555-5555
-            let del = delArr[2];
-            if (del === "" || del === " " || del === "-") {
-                return delArr[3] === del ? true : false
-            }
-            else { return false; }
-
-        case "(":
-            //Case 2: First character in str is "(". 2 valid formats.
-            //(555)555-5555 || (555) 555-5555
-            return delArr[3] === "-" && (delArr[2] === ")" || delArr[2] === ") ") ? true : false
-    
-        default: //If all else fails I mustv'e missed something, oops
-            return "\_^ ^_/";
+    else {
+        return rex2.test(str) ? true : false
     }
 }
 
-console.log(telephoneCheck(" -123) 456-7890"));
+console.log(telephoneCheck("1(123)456-7890"));
+
+//I wrote an explanation of why my solution is a little
+//better than the one provided by freeCodeCamp. Take a
+//look here: 
+//https://www.freecodecamp.org/forum/t/js-phone-validator-a-better-solution-imo/377825?u=ancheetah
+
+//(123)-456 7890 should be false
+//123 456-7890 should be false
+//123-456 7890 should be false
+//(555) 555 555 is now true

60d06873a1fec67bcef622ec96b7cae9dd42732a
AJ <ancheetah@users.noreply.github.com>
2020-04-29 23:29:33 -0400

    draft of a full solution. Need to run test cases.

--- javascript/projects/cash-register.js
+++ javascript/projects/cash-register.js
@@ -11,17 +11,40 @@ Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for th
 
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
 
-04/27/2020 To Do:
--If you dont have enough small change and break large bills then you must return "INSUFFICIENT_FUNDS"
+04/29/2020 To Do:
+-Test cases
 */
 
 function checkCashRegister(price, cash, cid) {
+  let len = cid.length,
+    change = cash - price;
+  //units = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]; //Note reversed  order from cid
+  //units = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
+  //let newcid = [...cid.reverse()]; //Keeps track of remaining cash in drawer after transaction
 
-  //Determine customer's change and total amount of cash in drawer
-  let change = cash - price,
-    total_cid = cid.reduce((sum, x) => sum += x[1], 0);
+  //Find largest denomination of cash due back to customer
+  //Determine denomination using "units" arr then map to index in "cid" arr
+  //let ind = (units.length - 1) - units.findIndex( (x,i,arr) => change >= x && change < arr[i+1] ); 
+  //let ind = units.findIndex( (x,i,arr) => change >= x && change < arr[i+1] ); 
+  //let ind = units.findIndex( x => x > change );
+  //console.log(ind);
 
-  //Check if there is enough cash in the drawer to give EXACT change
+  //Reverse search drawer to find largest denomination you can give change with
+  //A loop here is more efficient that using .reverse().findIndex()
+  let ind;
+  for (let j = cid.length - 1; j >= 0; j--) {
+    if (change >= cid[j][1]) {
+      ind = j;
+      break;
+    }
+  }
+
+  //Determine change owed to customer and total amount of cash in drawer
+  //let change = cash - price,
+  let total_cid = cid.reduce((sum, x, j) => (j <= ind) ? sum + x[1] : sum, 0); //Exclude bills/coins that are too big and can't be broken
+
+
+  //Check if there is enough cash in the drawer to give change
   if (total_cid < change) {
     return {
       status: "INSUFFICIENT_FUNDS",
@@ -37,81 +60,90 @@ function checkCashRegister(price, cash, cid) {
     };
   }
 
-  //Otherwise status is "Open." Continue to calculate change.
-  let ind, rem, cidElem, changeArr = [];
-  let units = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]; //Note reversed  order from cid
+  //Otherwise status is "Open." (cash in drawer is gt change)
+  //Remove change due and update cid
+  let rem, den
+  changeArr = [], //Customer's change
+    newcid = [...cid]; //Money left in drawer after transaction. Do we actually need this? No because we have already determined that 
+  let units = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
 
-  do {
-    //Determine largest USD denomination "change" is divisble by. Get corresponding index in units arr.
-    ind = units.findIndex(x => Math.trunc(change / x) != 0);
-    rem = change % units[ind]; //remaining change owed
-    cidElem = cid[cid.length - 1 - ind];
+  //Remove cash from drawer until there is no more change due to customer
+  while (change >= 0.01) { //smallest denomination = 0.01 = "PENNY"
+    //change = change.toPrecision(2);
+    //den = cid[ind][1]; //Amount of money in drawer at current denomination
 
-    //Validate funding
-    if (cidElem[1] < change - rem) { 
-      return {status: "INSUFFICIENT_FUNDS", change: []}; 
+    //If cid[ind] == 0 there are no bills/coins of the current denomination then skip to then break and decrement ind
+    if (cid[ind][1] == 0) {
+      ind--;
+      continue;
+    }
+    //If cid[ind] <= change then this denomination will be cleared out. Give the customer everything in cid[ind]. There may still be cash owed so keep iterating.
+    else if (cid[ind][1] <= change) {
+      changeArr.push(cid[ind]);
+      change -= cid[ind][1]; //Check if there is still changed owed
+    }
+    //If cid[ind] > change then return the bills owed. There may still be remaining cash owed which can only be returned with smaller denominations so keep iterating. ex) cid = $20x3; change = $41.50; return $20x2 + $1.50
+    else {  //cid[ind] > change
+      rem = change.toFixed(2) % units[ind]; //Remainder still owed. Round here to avoid precision errors with cents.
+      let withdrawn = change - rem; //Amount withdrawn from drawer
+      withdrawn = (withdrawn < 1) ? withdrawn.toFixed(2) : Math.round(withdrawn); //Formatting
+      if (withdrawn >= 0.01) { 
+        changeArr.push([cid[ind][0], withdrawn]) 
+        change = rem; //Update change owed with remainder
+      }; 
     }
 
-    //push with name and cash back
-    changeArr.push([cid[cid.length - 1 - ind][0], change - rem]);
-    change = rem; //update change with remaining change owed
+    ind--;
+
+
   }
-  while (change >= 0.01);
 
   return {
     status: "OPEN",
     change: changeArr
-  }
+  };
 }
-//   
+
+//Tests
 
 
+// console.log(checkCashRegister(20, 100, [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 0],
+//   ["ONE", 0],
+//   ["FIVE", 15],
+//   ["TEN", 0],
+//   ["TWENTY", 0],
+//   ["ONE HUNDRED", 200]
+// ]));
 
-//Update cid and status
+console.log(checkCashRegister(19.5,
+  20,
+  [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+  ]));
 
-//Find the remainder of change % lower range to get remaining change owed. Continue
-//until remainder is 0.
-
-
-//   if (change <= 0.01 && change > 0) { return {status: "OPEN", change: changeArr} }
-//   else 
-
-//   //Determine status of drawer
-//   //Rewrite change in terms of dollars
-
-//   //Return drawer status and change
-
-// return {
-//   status: "",
-//   change: []
-// };
-
-
-// console.log(checkCashRegister(19.5,
-//   20,
-//   [
-//     ["PENNY", 1.01],
-//     ["NICKEL", 2.05],
-//     ["DIME", 3.1],
-//     ["QUARTER", 4.25],
-//     ["ONE", 90],
-//     ["FIVE", 55],
-//     ["TEN", 20],
-//     ["TWENTY", 60],
-//     ["ONE HUNDRED", 100]
-//   ]));
-
-console.log(checkCashRegister(3.26, 100, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
-]));
+// console.log(checkCashRegister(3.26, 100, [
+//   ["PENNY", 1.01],
+//   ["NICKEL", 2.05],
+//   ["DIME", 3.1],
+//   ["QUARTER", 4.25],
+//   ["ONE", 90],
+//   ["FIVE", 55],
+//   ["TEN", 20],
+//   ["TWENTY", 60],
+//   ["ONE HUNDRED", 100]
+// ]));
 
 // console.log(checkCashRegister(19.5, 20, [
 //   ["PENNY", 0.01],
